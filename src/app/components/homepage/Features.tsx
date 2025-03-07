3b992cc67d52992af893a37ea7b8e2a116c97c78
arjunn2313 <arjunnks123@gmail.com>
2025-03-07 13:31:20 +0530

    modified features components

--- src/app/components/homepage/Features.tsx
+++ src/app/components/homepage/Features.tsx
@@ -26,7 +26,7 @@ const features = [
 export default function Features() {
   return (
     <section className="py-10 w-[95%] md:max-w-7xl   mx-auto">
-      <div className="  flex flex-wrap justify-center md:justify-between items-center gap-6 md:gap-0 py-4">
+      <div className="  flex w-[98%] mx-auto   flex-wrap justify-center md:justify-between items-center gap-6 md:gap-0 py-4">
         {features.map((feature, index) => (
           <div
             key={index}
